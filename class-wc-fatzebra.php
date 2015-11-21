8062268ffe0bf3dcd47e18fe113b2e0e80ac841f
Matthew Savage <matt@amasses.net>
2015-11-21 15:22:08 +1100

    Version bump

--- class-wc-fatzebra.php
+++ class-wc-fatzebra.php
@@ -4,7 +4,7 @@
 Plugin Name: WooCommerce Fat Zebra Gateway
 Plugin URI: https://www.fatzebra.com.au/support/supported-carts
 Description: Extends WooCommerce with Fat Zebra payment gateway along with WooCommerce subscriptions support.
-Version: 1.5.2
+Version: 1.5.3
 Author: Fat Zebra
 Author URI: https://www.fatzebra.com.au
 */
